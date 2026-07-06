24d57070d00f482eec3e1fbaa8a3d88b5ef2ed7e
happier <zhf5256@qq.com>
2026-07-06 15:18:29 +0800

    feat: 调整底部 tab 导航

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,34 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="songs" href="/tabs/songs">
+          <ion-icon aria-hidden="true" :icon="musicalNotes" />
+          <ion-label>歌曲</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="albums" href="/tabs/albums">
+          <ion-icon aria-hidden="true" :icon="albums" />
+          <ion-label>专辑</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="artists" href="/tabs/artists">
+          <ion-icon aria-hidden="true" :icon="person" />
+          <ion-label>艺术家</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="playlists" href="/tabs/playlists">
+          <ion-icon aria-hidden="true" :icon="list" />
+          <ion-label>歌单</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="sources" href="/tabs/sources">
+          <ion-icon aria-hidden="true" :icon="radio" />
+          <ion-label>音源</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="settings" href="/tabs/settings">
+          <ion-icon aria-hidden="true" :icon="settings" />
+          <ion-label>设置</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +39,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { albums, list, musicalNotes, person, radio, settings } from 'ionicons/icons';
 </script>
